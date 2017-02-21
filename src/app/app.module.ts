d36508d31fc322b57529440ad6961372f0494621
MrSkyBear <mrskybear@gmail.com>
2017-02-20 21:59:14 -0800

    Added routing module and factored out css files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { AppRoutingModule }     from './app-routing.module';
+
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+
 
 @NgModule({
   imports: [ 
+  	AppRoutingModule,
   	BrowserModule,
-  	FormsModule
+  	FormsModule,
   ],
   declarations: [ 
   	AppComponent,
-  	HeroDetailComponent
-   ],
+  	DashboardComponent,
+  	HeroDetailComponent,
+  	HeroesComponent
+  ],
+  providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
