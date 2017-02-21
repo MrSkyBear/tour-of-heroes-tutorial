4bc1241a5165b514f754eb7502f0bc0f15ffc586
MrSkyBear <mrskybear@gmail.com>
2017-02-20 23:12:30 -0800

    Implemented in-memory database and ability to add/update/delete heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule }     from './app-routing.module';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -9,12 +10,18 @@ import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 
 @NgModule({
   imports: [ 
   	AppRoutingModule,
   	BrowserModule,
   	FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   declarations: [ 
   	AppComponent,
